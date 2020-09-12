16c553d3c1781bdd52dd5f0230f49b1c9a4cad93
anurag2541997 <a.dixit@iiitmanipur.ac.in>
2020-09-12 14:08:12 +0530

    Updates

---
+++ precache-manifest.10c6c9cb255615f6d8f9f0903ff48c5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56167cfa9164e272b11d1e6896e4e172",
+    "url": "/countdown-timer-boilerplate/index.html"
+  },
+  {
+    "revision": "e7bcda7853f3d3fd7c67",
+    "url": "/countdown-timer-boilerplate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4af5b153d5df5a32d0a9",
+    "url": "/countdown-timer-boilerplate/static/js/2.afb06399.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown-timer-boilerplate/static/js/2.afb06399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7bcda7853f3d3fd7c67",
+    "url": "/countdown-timer-boilerplate/static/js/main.0a55e27e.chunk.js"
+  },
+  {
+    "revision": "3b9a2e4c3d48c28dd7d1",
+    "url": "/countdown-timer-boilerplate/static/js/runtime-main.6ef87fe9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/countdown-timer-boilerplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
