b158183602767ebff76608f3cb726ff5cc6acd32
anurag2541997 <a.dixit@iiitmanipur.ac.in>
2020-09-12 14:11:59 +0530

    Commit

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import Clock from './Clock';
 import CountdownForm from './CountdownForm';
-
 class Countdown extends React.Component {
-  
+    constructor(props){
+        super(props)
+        this.state={
+            count:0
+        }
+        /*this.update=this.update.bind(this)
+        this.resetstate=this.resetstate.bind(this)*/
+    }
+    update=(count)=>{
+        this.setState({
+            count: count
+        })
+        
+    }
+    resetstate=(count)=>{
+        this.setState({
+            count: 0
+        })
+    }
 
     render() {
-
+        
         return (
             <div>
-                <Clock timeInSeconds={count}/>
-                <CountdownForm onSetCountdownTime={}/>
+                { this.state.count!==0 ?  <Clock timeInSeconds={this.state.count} resettime={this.resetstate}/> : <h1>00:00</h1>}
+                <CountdownForm onSetCountdownTime={this.update}/>
             </div>
         );
     }
 }
-
-export default Countdown;
+export default Countdown
--- src/components/CountdownForm.jsx
+++ src/components/CountdownForm.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-
 class CountdownForm extends React.Component {
      onSubmit(e) {
-       //This will give you string for seconds. Do not remove refs
-        var secondsStr = this.refs.seconds.value;
-
-       
+        e.preventDefault()
+        var secondsStr = this.refs.seconds.value; 
+        if(secondsStr>0){
+        this.props.onSetCountdownTime(secondsStr)
+        this.refs.seconds.value="";
+        
+        }
     }
-
     render() {
         return (
             <div>
                 <form ref="form" onSubmit={this.onSubmit.bind(this)} className="countdown-form">
                     <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
-                    <input type="submit" className="button success expanded" value="Start Countdown"/>
+                    <input type="submit" className="button success expanded" value="Start Countdown"  />
                 </form>
             </div>
         );
